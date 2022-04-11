f023f148714ad75a4abff5e730b6ffae4e073830
JeannaMaye <54680474+Maye456@users.noreply.github.com>
2022-04-11 10:01:35 -0700

    Small Edits

--- src/main/java/com/gcu/ServletInitializer.java
+++ src/main/java/com/gcu/ServletInitializer.java
@@ -2,6 +2,8 @@ package com.gcu;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 
 public class ServletInitializer extends SpringBootServletInitializer {
 
@@ -9,5 +11,4 @@ public class ServletInitializer extends SpringBootServletInitializer {
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(LionQApplication.class);
     }
-
 }
